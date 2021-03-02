6e46973bc22a93872f19a9afffd48d6c1237e959
阿汤哥的晚餐 <1412899211@qq.com>
2021-03-02 08:59:59 +0800

    关于依赖

---
+++ mavena/src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+import com.entity.User;
+
+/**
+ * @author zzh
+ * @description
+ * @date
+ */
+public class Test {
+    public static void main(String[] args) {
+    User user=new User();
+    }
+}
---
+++ mavenb/src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import com.entity.Student;
+
+/**
+ * @author zzh
+ * @description
+ * @date
+ */
+public class Test {
+    public static void main(String[] args) {
+        Student student=new Student();
+    }
+}
